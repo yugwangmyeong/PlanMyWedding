115ea946c61a413a472e1bd698032ce595f424c9
yugwangmyeong <ykm5922@naver.com>
2025-04-04 15:40:06 +0900

    유광명

--- front/src/components/Community.jsx
+++ front/src/components/Community.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./styles/community.css";
 import toggleMenuIcon from "./styles/assets/toggleMenu.png";
-
+import Header from "./Header";
 const Community = () => {
   const [isMypageOpen, setIsMypageOpen] = useState(false);
 
@@ -39,38 +39,7 @@ const Community = () => {
   return (
     <div className="community-container">
       {/* 헤더 영역 */}
-     <header className="header">
-      <div className="logo">Plan my wedding</div>
-      <nav className="nav-links">
-        {/* 마이페이지 버튼 - 클릭 시 드롭다운 표시 */}
-        <div className={`mypage-container ${isMypageOpen ? "open" : ""}`}>
-          <button onClick={toggleMypageMenu} className="mypage-btn">
-            마이페이지 
-            <img
-             src={toggleMenuIcon}
-             className="mypage-icon" 
-             alt="toogle menu" 
-             />
-          </button>
-          <div className="mypage-dropdown">
-            <Link to="/schedule">일정관리</Link>
-            <Link to="/moneycontrol">예산관리</Link>
-          </div>
-        </div>
-        <Link to="/Community" className="header-margin">
-          커뮤니티
-        </Link>
-      </nav>
-
-      <nav className="nav-links2">
-        <Link to="/Login" className="login-link">로그인</Link>
-        <span className="header-margin">/ </span>
-        <Link to="/Register" className="reg-link">회원가입</Link>
-        <Link to="/Update" className="">
-          <span className="material-symbols-outlined">account_circle</span>
-        </Link>
-      </nav>
-    </header>
+     <Header></Header>
 
     {/* 커뮤니티 메인 콘텐츠 영역 */}
     <div className="community-content">
