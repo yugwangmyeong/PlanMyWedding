58b2c96140f567187164b43e7ebcf1f18fb7c582
yugwangmyeong <ykm5922@naver.com>
2025-04-08 17:35:04 +0900

    5:34

--- PlanMyWedding-main/PlanMyWedding-main/Backend/src/main/java/com/smhrd/myapp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smhrd.myapp.repository;
-
-import com.smhrd.myapp.User.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	Optional<User> findByEmail(String email);
-}
